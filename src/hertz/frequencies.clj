cb9200557c2212461efc135ecfb2174f5278494d
Ville Peurala <ville.peurala@gmail.com>
2018-09-19 13:01:43 +0300

    Some MIDI note numbers

--- src/hertz/frequencies.clj
+++ src/hertz/frequencies.clj
@@ -1,156 +1,157 @@
 (ns hertz.frequencies)
 
-(def freqs {
-            "C0"  16.35
-            "C#0" 17.32
-            "Db0" 17.32
-            "D0"  18.35
-            "D#0" 19.45
-            "Eb0" 19.45
-            "E0"  20.60
-            "F0"  21.83
-            "F#0" 23.12
-            "Gb0" 23.12
-            "G0"  24.50
-            "G#0" 25.96
-            "Ab0" 25.96
-            "A0"  27.50
-            "A#0" 29.14
-            "Bb0" 29.14
-            "B0"  30.87
-            "C1"  32.70
-            "C#1" 34.65
-            "D1"  36.71
-            "D#1" 38.89
-            "Eb1" 38.89
-            "E1"  41.20
-            "F1"  43.65
-            "F#1" 46.25
-            "Gb1" 46.25
-            "G1"  49.00
-            "G#1" 51.91
-            "Ab1" 51.91
-            "A1"  55.00
-            "A#1" 58.27
-            "Bb1" 58.27
-            "B1"  61.74
-            "C2"  65.41
-            "C#2" 69.30
-            "Db2" 69.30
-            "D2"  73.42
-            "D#2" 77.78
-            "Eb2" 77.78
-            "E2"  82.41
-            "F2"  87.31
-            "F#2" 92.50
-            "Gb2" 92.50
-            "G2"  98.00
-            "G#2" 103.83
-            "Ab2" 103.83
-            "A2"  110.00
-            "A#2" 116.54
-            "Bb2" 116.54
-            "B2"  123.47
-            "C3"  130.81
-            "C#3" 138.59
-            "Db3" 138.59
-            "D3"  146.83
-            "D#3" 155.56
-            "Eb3" 155.56
-            "E3"  164.81
-            "F3"  174.61
-            "F#3" 185.00
-            "Gb3" 185.00
-            "G3"  196.00
-            "G#3" 207.65
-            "Ab3" 207.65
-            "A3"  220.00
-            "A#3" 233.08
-            "Bb3" 233.08
-            "B3"  246.94
-            "C4"  261.63
-            "C#4" 277.18
-            "Db4" 277.18
-            "D4"  293.66
-            "D#4" 311.13
-            "Eb4" 311.13
-            "E4"  329.63
-            "F4"  349.23
-            "F#"  369.99
-            "Gb4" 369.99
-            "G4"  392.00
-            "G#4" 415.30
-            "Ab4" 415.30
-            "A4"  440.00
-            "A#4" 466.16
-            "Bb4" 466.16
-            "B4"  493.88
-            "C5"  523.25
-            "C#5" 554.37
-            "Db5" 554.37
-            "D5"  587.33
-            "D#5" 622.25
-            "Eb5" 622.25
-            "E5"  659.25
-            "F5"  698.46
-            "F#5" 739.99
-            "Gb5" 739.99
-            "G5"  783.99
-            "G#5" 830.61
-            "Ab5" 830.61
-            "A5"  880.00
-            "A#5" 932.33
-            "Bb5" 932.33
-            "B5"  987.77
-            "C6"  1046.50
-            "C#6" 1108.73
-            "Db6" 1108.73
-            "D6"  1174.66
-            "D#6" 1244.51
-            "Eb6" 1244.51
-            "E6"  1318.51
-            "F6"  1396.91
-            "F#6" 1479.98
-            "Gb6" 1479.98
-            "G6"  1567.98
-            "G#6" 1661.22
-            "Ab6" 1661.22
-            "A6"  1760.00
-            "A#6" 1864.66
-            "Bb6" 1864.66
-            "B6"  1975.53
-            "C7"  2093.00
-            "C#7" 2217.46
-            "Db7" 2217.46
-            "D7"  2349.32
-            "D#7" 2489.02
-            "Eb7" 2489.02
-            "E7"  2637.02
-            "F7"  2793.83
-            "F#7" 2959.96
-            "Gb7" 2959.96
-            "G7"  3135.96
-            "G#7" 3322.44
-            "Ab7" 3322.44
-            "A7"  3520.00
-            "A#7" 3729.31
-            "Bb7" 3729.31
-            "B7"  3951.07
-            "C8"  4186.01
-            "C#8" 4434.92
-            "Db8" 4434.92
-            "D8"  4698.63
-            "D#8" 4978.03
-            "Eb8" 4978.03
-            "E8"  5274.04
-            "F8"  5587.65
-            "F#"  5919.91
-            "Gb8" 5919.91
-            "G8"  6271.93
-            "G#8" 6644.88
-            "Ab8" 6644.88
-            "A8"  7040.00
-            "A#8" 7458.62
-            "Bb8" 7458.62
-            "B8"  7902.13
-            })
+(def freqs [
+            {:note-name "C0" :frequency 16.35 :midi-note-number 12}
+            {:note-name "C#0" :frequency 17.32 :midi-note-number 13}
+            {:note-name "Db0" :frequency 17.32 :midi-note-number 13}
+            {:note-name "D0" :frequency 18.35 :midi-note-number 14}
+            {:note-name "D#0" :frequency 19.45 :midi-note-number 15}
+            {:note-name "Eb0" :frequency 19.45 :midi-note-number 15}
+            {:note-name "E0" :frequency 20.60 :midi-note-number 16}
+            {:note-name "F0" :frequency 21.83 :midi-note-number 17}
+            {:note-name "F#0" :frequency 23.12 :midi-note-number 18}
+            {:note-name "Gb0" :frequency 23.12 :midi-note-number 18}
+            {:note-name "G0" :frequency 24.50 :midi-note-number 19}
+            {:note-name "G#0" :frequency 25.96 :midi-note-number 20}
+            {:note-name "Ab0" :frequency 25.96 :midi-note-number 20}
+            {:note-name "A0" :frequency 27.50 :midi-note-number 21}
+            {:note-name "A#0" :frequency 29.14 :midi-note-number 22}
+            {:note-name "Bb0" :frequency 29.14 :midi-note-number 22}
+            {:note-name "B0" :frequency 30.87 :midi-note-number 23}
+            {:note-name "C1" :frequency 32.70 :midi-note-number 24}
+            {:note-name "C#1" :frequency 34.65 :midi-note-number 25}
+            {:note-name "Db1" :frequency 34.65 :midi-note-number 25}
+            {:note-name "D1" :frequency 36.71 :midi-note-number 26}
+            {:note-name "D#1" :frequency 38.89 :midi-note-number 27}
+            {:note-name "Eb1" :frequency 38.89 :midi-note-number 27}
+            {:note-name "E1" :frequency 41.20 :midi-note-number 28}
+            {:note-name "F1" :frequency 43.65 :midi-note-number 29}
+            {:note-name "F#1" :frequency 46.25 :midi-note-number 30}
+            {:note-name "Gb1" :frequency 46.25 :midi-note-number 30}
+            {:note-name "G1" :frequency 49.00 :midi-note-number 31}
+            {:note-name "G#1" :frequency 51.91 :midi-note-number 32}
+            {:note-name "Ab1" :frequency 51.91 :midi-note-number 32}
+            {:note-name "A1" :frequency 55.00 :midi-note-number 33}
+            {:note-name "A#1" :frequency 58.27 :midi-note-number 34}
+            {:note-name "Bb1" :frequency 58.27 :midi-note-number 34}
+            {:note-name "B1" :frequency 61.74 :midi-note-number 35}
+            {:note-name "C2" :frequency 65.41 :midi-note-number 36}
+            {:note-name "C#2" :frequency 69.30 :midi-note-number 1}
+            {:note-name "Db2" :frequency 69.30 :midi-note-number 1}
+            {:note-name "D2" :frequency 73.42 :midi-note-number 1}
+            {:note-name "D#2" :frequency 77.78 :midi-note-number 1}
+            {:note-name "Eb2" :frequency 77.78 :midi-note-number 1}
+            {:note-name "E2" :frequency 82.41 :midi-note-number 1}
+            {:note-name "F2" :frequency 87.31 :midi-note-number 1}
+            {:note-name "F#2" :frequency 92.50 :midi-note-number 1}
+            {:note-name "Gb2" :frequency 92.50 :midi-note-number 1}
+            {:note-name "G2" :frequency 98.00 :midi-note-number 1}
+            {:note-name "G#2" :frequency 103.83 :midi-note-number 1}
+            {:note-name "Ab2" :frequency 103.83 :midi-note-number 1}
+            {:note-name "A2" :frequency 110.00 :midi-note-number 1}
+            {:note-name "A#2" :frequency 116.54 :midi-note-number 1}
+            {:note-name "Bb2" :frequency 116.54 :midi-note-number 1}
+            {:note-name "B2" :frequency 123.47 :midi-note-number 1}
+            {:note-name "C3" :frequency 130.81 :midi-note-number 1}
+            {:note-name "C#3" :frequency 138.59 :midi-note-number 1}
+            {:note-name "Db3" :frequency 138.59 :midi-note-number 1}
+            {:note-name "D3" :frequency 146.83 :midi-note-number 1}
+            {:note-name "D#3" :frequency 155.56 :midi-note-number 1}
+            {:note-name "Eb3" :frequency 155.56 :midi-note-number 1}
+            {:note-name "E3" :frequency 164.81 :midi-note-number 1}
+            {:note-name "F3" :frequency 174.61 :midi-note-number 1}
+            {:note-name "F#3" :frequency 185.00 :midi-note-number 1}
+            {:note-name "Gb3" :frequency 185.00 :midi-note-number 1}
+            {:note-name "G3" :frequency 196.00 :midi-note-number 1}
+            {:note-name "G#3" :frequency 207.65 :midi-note-number 1}
+            {:note-name "Ab3" :frequency 207.65 :midi-note-number 1}
+            {:note-name "A3" :frequency 220.00 :midi-note-number 1}
+            {:note-name "A#3" :frequency 233.08 :midi-note-number 1}
+            {:note-name "Bb3" :frequency 233.08 :midi-note-number 1}
+            {:note-name "B3" :frequency 246.94 :midi-note-number 1}
+            {:note-name "C4" :frequency 261.63 :midi-note-number 1}
+            {:note-name "C#4" :frequency 277.18 :midi-note-number 1}
+            {:note-name "Db4" :frequency 277.18 :midi-note-number 1}
+            {:note-name "D4" :frequency 293.66 :midi-note-number 1}
+            {:note-name "D#4" :frequency 311.13 :midi-note-number 1}
+            {:note-name "Eb4" :frequency 311.13 :midi-note-number 1}
+            {:note-name "E4" :frequency 329.63 :midi-note-number 1}
+            {:note-name "F4" :frequency 349.23 :midi-note-number 1}
+            {:note-name "F#" :frequency 369.99 :midi-note-number 1}
+            {:note-name "Gb4" :frequency 369.99 :midi-note-number 1}
+            {:note-name "G4" :frequency 392.00 :midi-note-number 1}
+            {:note-name "G#4" :frequency 415.30 :midi-note-number 1}
+            {:note-name "Ab4" :frequency 415.30 :midi-note-number 1}
+            {:note-name "A4" :frequency 440.00 :midi-note-number 1}
+            {:note-name "A#4" :frequency 466.16 :midi-note-number 1}
+            {:note-name "Bb4" :frequency 466.16 :midi-note-number 1}
+            {:note-name "B4" :frequency 493.88 :midi-note-number 1}
+            {:note-name "C5" :frequency 523.25 :midi-note-number 1}
+            {:note-name "C#5" :frequency 554.37 :midi-note-number 1}
+            {:note-name "Db5" :frequency 554.37 :midi-note-number 1}
+            {:note-name "D5" :frequency 587.33 :midi-note-number 1}
+            {:note-name "D#5" :frequency 622.25 :midi-note-number 1}
+            {:note-name "Eb5" :frequency 622.25 :midi-note-number 1}
+            {:note-name "E5" :frequency 659.25 :midi-note-number 1}
+            {:note-name "F5" :frequency 698.46 :midi-note-number 1}
+            {:note-name "F#5" :frequency 739.99 :midi-note-number 1}
+            {:note-name "Gb5" :frequency 739.99 :midi-note-number 1}
+            {:note-name "G5" :frequency 783.99 :midi-note-number 1}
+            {:note-name "G#5" :frequency 830.61 :midi-note-number 1}
+            {:note-name "Ab5" :frequency 830.61 :midi-note-number 1}
+            {:note-name "A5" :frequency 880.00 :midi-note-number 1}
+            {:note-name "A#5" :frequency 932.33 :midi-note-number 1}
+            {:note-name "Bb5" :frequency 932.33 :midi-note-number 1}
+            {:note-name "B5" :frequency 987.77 :midi-note-number 1}
+            {:note-name "C6" :frequency 1046.50 :midi-note-number 1}
+            {:note-name "C#6" :frequency 1108.73 :midi-note-number 1}
+            {:note-name "Db6" :frequency 1108.73 :midi-note-number 1}
+            {:note-name "D6" :frequency 1174.66 :midi-note-number 1}
+            {:note-name "D#6" :frequency 1244.51 :midi-note-number 1}
+            {:note-name "Eb6" :frequency 1244.51 :midi-note-number 1}
+            {:note-name "E6" :frequency 1318.51 :midi-note-number 1}
+            {:note-name "F6" :frequency 1396.91 :midi-note-number 1}
+            {:note-name "F#6" :frequency 1479.98 :midi-note-number 1}
+            {:note-name "Gb6" :frequency 1479.98 :midi-note-number 1}
+            {:note-name "G6" :frequency 1567.98 :midi-note-number 1}
+            {:note-name "G#6" :frequency 1661.22 :midi-note-number 1}
+            {:note-name "Ab6" :frequency 1661.22 :midi-note-number 1}
+            {:note-name "A6" :frequency 1760.00 :midi-note-number 1}
+            {:note-name "A#6" :frequency 1864.66 :midi-note-number 1}
+            {:note-name "Bb6" :frequency 1864.66 :midi-note-number 1}
+            {:note-name "B6" :frequency 1975.53 :midi-note-number 1}
+            {:note-name "C7" :frequency 2093.00 :midi-note-number 1}
+            {:note-name "C#7" :frequency 2217.46 :midi-note-number 1}
+            {:note-name "Db7" :frequency 2217.46 :midi-note-number 1}
+            {:note-name "D7" :frequency 2349.32 :midi-note-number 1}
+            {:note-name "D#7" :frequency 2489.02 :midi-note-number 1}
+            {:note-name "Eb7" :frequency 2489.02 :midi-note-number 1}
+            {:note-name "E7" :frequency 2637.02 :midi-note-number 1}
+            {:note-name "F7" :frequency 2793.83 :midi-note-number 1}
+            {:note-name "F#7" :frequency 2959.96 :midi-note-number 1}
+            {:note-name "Gb7" :frequency 2959.96 :midi-note-number 1}
+            {:note-name "G7" :frequency 3135.96 :midi-note-number 1}
+            {:note-name "G#7" :frequency 3322.44 :midi-note-number 1}
+            {:note-name "Ab7" :frequency 3322.44 :midi-note-number 1}
+            {:note-name "A7" :frequency 3520.00 :midi-note-number 1}
+            {:note-name "A#7" :frequency 3729.31 :midi-note-number 1}
+            {:note-name "Bb7" :frequency 3729.31 :midi-note-number 1}
+            {:note-name "B7" :frequency 3951.07 :midi-note-number 1}
+            {:note-name "C8" :frequency 4186.01 :midi-note-number 1}
+            {:note-name "C#8" :frequency 4434.92 :midi-note-number 1}
+            {:note-name "Db8" :frequency 4434.92 :midi-note-number 1}
+            {:note-name "D8" :frequency 4698.63 :midi-note-number 1}
+            {:note-name "D#8" :frequency 4978.03 :midi-note-number 1}
+            {:note-name "Eb8" :frequency 4978.03 :midi-note-number 1}
+            {:note-name "E8" :frequency 5274.04 :midi-note-number 1}
+            {:note-name "F8" :frequency 5587.65 :midi-note-number 1}
+            {:note-name "F#" :frequency 5919.91 :midi-note-number 1}
+            {:note-name "Gb8" :frequency 5919.91 :midi-note-number 1}
+            {:note-name "G8" :frequency 6271.93 :midi-note-number 1}
+            {:note-name "G#8" :frequency 6644.88 :midi-note-number 1}
+            {:note-name "Ab8" :frequency 6644.88 :midi-note-number 1}
+            {:note-name "A8" :frequency 7040.00 :midi-note-number 1}
+            {:note-name "A#8" :frequency 7458.62 :midi-note-number 1}
+            {:note-name "Bb8" :frequency 7458.62 :midi-note-number 1}
+            {:note-name "B8" :frequency 7902.13 :midi-note-number 1}
+            ])
