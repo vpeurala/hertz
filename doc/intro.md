f46a884043404c5141af051165953dd7e52aa847
Ville Peurala <ville.peurala@gmail.com>
2018-09-19 12:15:24 +0300

    Project skeleton from Leiningen's app template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hertz
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hertz/core_test.clj
@@ -0,0 +1,7 @@
+(ns hertz.core-test
+  (:require [clojure.test :refer :all]
+            [hertz.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
