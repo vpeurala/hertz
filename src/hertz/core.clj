4ea4004133236513ac69ceb80ee8f965d1c9b434
Ville Peurala <ville.peurala@gmail.com>
2018-09-19 13:26:41 +0300

    Very simple question/answer loop

--- src/hertz/core.clj
+++ src/hertz/core.clj
@@ -1,7 +1,22 @@
 (ns hertz.core
+  (:require [clojure.edn :refer [read-string]]
+            [hertz.frequencies :as f])
   (:gen-class))
 
 (defn -main
   [& _]
   (println "Hertz")
-  (println "-----"))
+  (println "-----")
+  (loop []
+    (let [index (rand-int (count f/freqs))
+          note (nth f/freqs index)]
+      (println (format "Frequency of note %s?" (:note-name note)))
+      (let [answer (read-string (read-line))]
+        (if (number? answer)
+          (if
+            (= (double answer) (:frequency note))
+            (println "Correct!")
+            (println (format "Wrong! The correct frequency is %.2f!" (:frequency note)))
+            )
+          (println "Incorrect answer format, number expected."))))
+    (recur)))
