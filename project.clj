13ae65fa818056a7e9a47567b114c7b54a88b5fb
Ville Peurala <ville.peurala@gmail.com>
2018-09-20 12:19:01 +0300

    Use colors

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[clojure-term-colors "0.1.0"]
+                 [org.clojure/clojure "1.9.0"]
                  [overtone "0.10.3"]
                  [try-let "1.2.0"]]
   :main ^:skip-aot hertz.core
--- src/hertz/core.clj
+++ src/hertz/core.clj
@@ -1,5 +1,6 @@
 (ns hertz.core
   (:require [clojure.edn :as edn]
+            [clojure.term.colors :as c]
             [hertz.frequencies :as f]
             [overtone.live :as otl]
             [try-let :refer [try-let]])
@@ -8,22 +9,20 @@
 
 (defn -main
   [& _]
-  (println "Hertz")
-  (println "-----")
+  (println (c/yellow (c/underline "Hertz")))
   (loop []
-    (let [index (rand-int (count f/freqs))
-          note (nth f/freqs index)]
+    (let [note (rand-nth f/freqs)]
       (println (format "Frequency of note %s?" (:note-name note)))
       (otl/demo 2 (otl/sin-osc (:frequency note) 0.0 1 0))
       (try-let [answer (edn/read-string (read-line))]
                (if (number? answer)
                  (if
                    (= (double answer) (:frequency note))
-                   (println "Correct!")
-                   (println
-                     (format "Wrong! The correct frequency for %s is %.2f!"
-                             (:note-name note)
-                             (:frequency note))))
-                 (println "Incorrect answer format, number expected."))
-               (catch NumberFormatException e (println "Incorrect answer format, number expected."))))
+                   (println (c/green "Correct!"))
+                   (println (c/red
+                              (format "Wrong! The correct frequency for %s is %.2f!"
+                                      (:note-name note)
+                                      (:frequency note)))))
+                 (println (c/red (c/bold "Incorrect answer format, number expected."))))
+               (catch NumberFormatException _ (println (c/red (c/bold "Incorrect answer format, number expected."))))))
     (recur)))
